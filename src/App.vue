d4bc11d33583f5c2f1ceb31f2bf265ad52e710e3
小让 <15019474951@163.com>
2025-05-05 23:36:44 +0800

    feat: integrate pinia as state management

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,17 @@
   <div>
     <RouterView />
   </div>
+
+  <button @click="decrement">-1</button>
+  {{ count }}
+  <button @click="increment">+1</button>
 </template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from './stores/counter'
+
+const counterStore = useCounterStore()
+const { count } = storeToRefs(counterStore)
+const { increment, decrement } = counterStore
+</script>
